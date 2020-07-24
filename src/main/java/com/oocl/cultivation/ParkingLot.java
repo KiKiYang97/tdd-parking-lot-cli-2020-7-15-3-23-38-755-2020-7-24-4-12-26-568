fbb82142d6d030eabd2bd588038d2709cd2d6898
YANGKI4 <dunka97@163.com>
2020-07-24 17:29:04 +0800

    [ParkingLot][KikiYang]refactor:refactor the parkinglot , add a field:capacity

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,12 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
-    private Map<CarTicket,Car> parkingLotMap = new HashMap<CarTicket,Car>();
+    private Map<CarTicket, Car> parkingLotMap;
+    private int capacity;
+
+    public ParkingLot() {
+        this.capacity = 10;
+        this.parkingLotMap = new HashMap<>();
+    }
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
+
     public CarTicket park(Car car) {
-        if (this.parkingLotMap.size()==10)
+        if (this.parkingLotMap.size() < this.capacity)
             return null;
         CarTicket ticket = new CarTicket();
-        parkingLotMap.put(ticket,car);
+        parkingLotMap.put(ticket, car);
         return ticket;
     }
 
